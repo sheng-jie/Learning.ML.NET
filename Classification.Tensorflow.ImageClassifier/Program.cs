81aee5f6f45df96389efb8db99b9f49fb7124c8c
shengjie_yan <ysjshengjie@live.cn>
2018-12-21 09:43:04 +0800

    Use tensorflow model to classify iamges

--- Classification.Tensorflow.ImageClassifier/Program.cs
+++ Classification.Tensorflow.ImageClassifier/Program.cs
@@ -1,25 +1,29 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using Microsoft.ML;
 using Microsoft.ML.Data;
 using Microsoft.ML.Runtime.Data;
 using Microsoft.ML.Runtime.ImageAnalytics;
+using Microsoft.ML.Trainers.FastTree.Internal;
 using Microsoft.ML.Transforms;
 
 namespace Classification.Tensorflow.ImageClassifier
 {
     class Program
     {
-        private static string DataPath = Path.Combine(Environment.CurrentDirectory, "Data");
-        private static string ImageFolder = Path.Combine(Environment.CurrentDirectory, "Data");
-        private static string TFModelPath = Path.Combine(Environment.CurrentDirectory, "Data");
-        private static string TestDataPath = Path.Combine(Environment.CurrentDirectory, "Data");
-        
+        private static string RootPath = Path.GetFullPath(@"..\..\..\");
+        private static string DataPath = Path.Combine(RootPath, "Data", "Images", "tags.tsv");
+        private static string ImageFolder = Path.Combine(RootPath, "Data", "Images");
+        private static string TFModelPath = Path.Combine(RootPath, "Data", "TFModel", "tensorflow_inception_graph.pb");
+        private static string TFModelLabelPath = Path.Combine(RootPath, "Data", "TFModel",
+            "imagenet_comp_graph_label_strings.txt");
+
         static void Main(string[] args)
         {
             // 创建上下文
             var mlContext = new MLContext();
-            
+
             var loader = new TextLoader(mlContext, new TextLoader.Arguments()
             {
                 Column = new[]
@@ -40,13 +44,14 @@ namespace Classification.Tensorflow.ImageClassifier
                         new ImagePixelExtractorTransform.ColumnInfo("ImageReal", "input",
                             interleave: ImageNetSettings.ChannelsLast, offset: ImageNetSettings.Mean),
                     }))
-                    .Append(mlContext.Transforms.ScoreTensorFlowModel(TFModelPath, new[] {"input"},
-                        new[] {"softmax2"}));
+                    .Append(mlContext.Transforms.ScoreTensorFlowModel(TFModelPath, new[] { "input" },
+                        new[] { "softmax2" }));
             var model = pipeline.Fit(data);
 
             var predictionFunc = model.MakePredictionFunction<ImageNetData, ImageNetPrediction>(mlContext);
 
-            var testData = ImageNetData.ReadFromCsv(TestDataPath, ImageFolder);
+            var testData = ImageNetData.ReadFromCsv(DataPath, ImageFolder);
+            var labels = File.ReadAllLines(TFModelLabelPath);
 
             foreach (var sample in testData)
             {
@@ -56,9 +61,52 @@ namespace Classification.Tensorflow.ImageClassifier
                     ImagePath = sample.ImagePath,
                     Label = sample.Label
                 };
-            }
 
-            Console.WriteLine("Hello World!");
+                (imageData.PredicateLabel, imageData.Probability) = GetBestLabel(labels, probs);
+                PrintReuslt(imageData);
+            }
+        }
+
+        public static (string, float) GetBestLabel(string[] labels, float[] probs)
+        {
+            var max = probs.Max();
+            var index = probs.AsSpan().IndexOf(max);
+            return (labels[index], max);
+        }
+
+        public static void PrintReuslt(ImageNetDataProbability predictionResult)
+        {
+            var defaultForeground = Console.ForegroundColor;
+            var labelColor = ConsoleColor.Magenta;
+            var probColor = ConsoleColor.Cyan;
+            var exactLabel = ConsoleColor.Green;
+            var failLabel = ConsoleColor.Red;
+
+            Console.Write("ImagePath: ");
+            Console.ForegroundColor = labelColor;
+            Console.Write($"{Path.GetFileName(predictionResult.ImagePath)}");
+            Console.ForegroundColor = defaultForeground;
+            Console.Write(" labeled as ");
+            Console.ForegroundColor = labelColor;
+            Console.Write(predictionResult.Label);
+            Console.ForegroundColor = defaultForeground;
+            Console.Write(" predicted as ");
+            if (predictionResult.Label.Equals(predictionResult.PredicateLabel))
+            {
+                Console.ForegroundColor = exactLabel;
+                Console.Write($"{predictionResult.PredicateLabel}");
+            }
+            else
+            {
+                Console.ForegroundColor = failLabel;
+                Console.Write($"{predictionResult.PredicateLabel}");
+            }
+            Console.ForegroundColor = defaultForeground;
+            Console.Write(" with probability ");
+            Console.ForegroundColor = probColor;
+            Console.Write(predictionResult.Probability);
+            Console.ForegroundColor = defaultForeground;
+            Console.WriteLine("");
         }
     }
 }
